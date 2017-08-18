44c24237f53650f2ee7f3f3add8ab1b3e3032546
defo10 <daniel.t.stachnik@gmail.com>
2017-08-18 16:59:40 +0200

    1.0

--- app/src/main/java/com/example/android/newz/MainActivity.java
+++ app/src/main/java/com/example/android/newz/MainActivity.java
@@ -5,7 +5,6 @@ import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.Menu;
